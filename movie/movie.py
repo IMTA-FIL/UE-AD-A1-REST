09bd58beb9ddd8e5fa2aeffd40894e563b72c7d6
Coullon Helene <helene.coullon@inria.fr>
2022-09-05 18:11:04 +0200

    Update movie.py

--- movie/movie.py
+++ movie/movie.py
@@ -16,18 +16,6 @@ with open('{}/databases/movies.json'.format("."), "r") as jsf:
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
-# to test templates of Flask
-@app.route("/template", methods=['GET'])
-def template():
-    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'),200)
-
-# get the complete json file
-@app.route("/json", methods=['GET'])
-def get_json():
-    #res = make_response(jsonify(INFO), 200)
-    res = make_response(jsonify(movies), 200)
-    return res
-
 if __name__ == "__main__":
     #p = sys.argv[1]
     print("Server running in port %s"%(PORT))
