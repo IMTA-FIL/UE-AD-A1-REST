cb1e2c04ab770a58a2a12dad16221d4caaa9cd5a
Helene Coullon <helene.coullon@inria.fr>
2024-08-28 15:00:59 +0200

    update 24-25

--- movie/movie.py
+++ movie/movie.py
@@ -8,13 +8,87 @@ app = Flask(__name__)
 PORT = 3200
 HOST = '0.0.0.0'
 
-with open('{}/databases/movies.json'.format("."), "r") as jsf:
+with open('{}/databases/movies.json'.format("."), 'r') as jsf:
    movies = json.load(jsf)["movies"]
+
+def write(movies):
+    with open('{}/databases/movies.json'.format("."), 'w') as f:
+        dict = {}
+        dict['movies']=movies
+        json.dump(dict, f)
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+
+@app.route("/template", methods=['GET'])
+def template():
+    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'),200)
+
+@app.route("/json", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(movies), 200)
+    return res
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie),200)
+            return res
+    return make_response(jsonify({"error":"Movie ID not found"}),400)
+
+@app.route("/moviesbytitle", methods=['GET'])
+def get_movie_bytitle():
+    print("test")
+    json = ""
+    if request.args:
+        req = request.args
+        for movie in movies:
+            if str(movie["title"]) == str(req["title"]):
+                json = movie
+
+    if not json:
+        res = make_response(jsonify({"error":"movie title not found"}),400)
+    else:
+        res = make_response(jsonify(json),200)
+    return res
+
+@app.route("/addmovie/<movieid>", methods=['POST'])
+def add_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            return make_response(jsonify({"error":"movie ID already exists"}),409)
+
+    movies.append(req)
+    write(movies)
+    res = make_response(jsonify({"message":"movie added"}),200)
+    return res
+
+@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+def update_movie_rating(movieid, rate):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movie["rating"] = rate
+            res = make_response(jsonify(movie),200)
+            return res
+
+    write(movies)
+    res = make_response(jsonify({"error":"movie ID not found"}),201)
+    return res
+
+@app.route("/movies/<movieid>", methods=['DELETE'])
+def del_movie(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movies.remove(movie)
+            return make_response(jsonify(movie),200)
+
+    res = make_response(jsonify({"error":"movie ID not found"}),400)
+    return res
 
 if __name__ == "__main__":
     #p = sys.argv[1]
