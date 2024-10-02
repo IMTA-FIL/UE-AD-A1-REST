8676856b1d943f59358b346142f02bee4f041123
NicolasChusseau <nchusseau@free.fr>
2024-10-02 17:08:36 +0200

    showtime implementation

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -8,12 +8,47 @@ PORT = 3202
 HOST = '0.0.0.0'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
-   schedule = json.load(jsf)["schedule"]
+    schedules = json.load(jsf)["schedule"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+
+@app.route("/showtimes", methods=['GET'])
+def get_schedules():
+    json = jsonify(schedules)
+    res = make_response(json, 200)
+    return res
+
+def is_invalide_date(date):
+    """
+    only basic validation are made here
+    we don't check is the date is a valid date, only if it's a valid string
+    """
+    if len(date) != 8:
+        return True
+    if not date.isdigit():
+        return True
+    return False
+
+
+@app.route("/showmovies/<date>")
+def get_schedules_by_date(date):
+    if is_invalide_date(date):
+        error_text = {"error": "bad input parameter"}
+        response = make_response(jsonify(error_text), 400)
+        return response
+
+    json = []
+    for schedule in schedules:
+        if schedule["date"] == date:
+            json.append(schedule)
+    res = make_response(jsonify(json), 200)
+    return res
+
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
