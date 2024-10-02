4bee6de37152fe21da5f91d6d8443fb61803b6e5
NicolasChusseau <nchusseau@free.fr>
2024-10-02 19:11:01 +0200

    ajout des routes en get

--- booking/booking.py
+++ booking/booking.py
@@ -9,13 +9,40 @@ PORT = 3201
 HOST = '0.0.0.0'
 
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
-   bookings = json.load(jsf)["bookings"]
+    bookings = json.load(jsf)["bookings"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+
+@app.route("/bookings", methods=['GET'])
+def get_bookings():
+    json = jsonify(bookings)
+    response = make_response(json, 200)
+    return response
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_from_userid(userid):
+    json = ""
+    for booking in bookings:
+        if booking['userid'] == userid:
+            json = booking
+
+    if not json:
+        response = make_response(jsonify({"error" : "bad input parameter"}), 400)
+    else :
+        response = make_response(jsonify(json), 200)
+    return response
+
+
+def write(bookings):
+    data = {"bookings": bookings}
+    with open('./databases/bookings.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
