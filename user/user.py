1a325a73693f49e64228a36455aaf1b10afee804
NicolasChusseau <nchusseau@free.fr>
2024-10-04 11:56:02 +0200

    début branche

--- user/user.py
+++ user/user.py
@@ -9,13 +9,30 @@ PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+
+@app.route("/users", methods=['GET'])
+def get_users():
+    # return all users
+    pass
+
+
+@app.route("/users/<userid>", methods=['GET'])
+def get_user(userid):
+    # return user with id
+    pass
+
+# récupérer tous les bookings d'un user (lien avec booking)
+
+# même chose mais en récupérant aussi les infos des films (lien avec booking et movie)
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
