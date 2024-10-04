21070931db6d05215021dbd88e8f3b3ebf50317b
Marc <m.underray@gmail.com>
2024-10-04 10:50:27 +0200

    Users

--- user/user.py
+++ user/user.py
@@ -21,6 +21,29 @@ def home():
 def user():
 	return make_response(jsonify(users), 200)
 
+@app.route("/users/<userid>", methods=['POST'])
+def add_user(userid):
+    req = request.get_json()
+    for u in users:
+        if str(u['id']) == str(userid):
+            return make_response(jsonify({'error': 'User ID already exists'}), 409)
+    users.append(req)
+    write(users)
+    res = make_response(jsonify({"message":"user added"}),200)
+    return res
+
+@app.route('/users/<userid>', methods=['DELETE'])
+def delete_movie(userid):
+    for u in users:
+        if str(u['id']) == str(userid):
+            users.remove(u)
+            write(users)
+            return make_response(jsonify({'message': 'Deleted successfully'}), 200)
+    return make_response(jsonify({'error': 'User not found'}), 400)
+
+def write(users):
+    with open('{}/databases/users.json'.format("."), 'w') as f:
+        json.dump({'users':users}, f, indent=4)
 
 if __name__ == "__main__":
 	print("Server running in port %s"%(PORT))
