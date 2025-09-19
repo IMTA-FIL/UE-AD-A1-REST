2589d13215d5f433b6a586e6accef9fc8b2dc846
Helene Coullon <hcoullon@PA-DAPI-NA-SALSA-008.local>
2025-09-19 10:32:16 +0200

    fix movie.py

--- movie/movie.py
+++ movie/movie.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 import json
 import sys
 from werkzeug.exceptions import NotFound
@@ -9,7 +9,14 @@ PORT = 3200
 HOST = '0.0.0.0'
 
 with open('{}/databases/movies.json'.format("."), 'r') as jsf:
-   movies = json.load(jsf)["movies"]
+    movies = json.load(jsf)["movies"]
+    print(movies)
+
+def write(movies):
+    with open('{}/databases/movies.json'.format("."), 'w') as f:
+        full = {}
+        full['movies']=movies
+        json.dump(full, f)
 
 # root message
 @app.route("/", methods=['GET'])
