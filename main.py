1c7972243e258e591b9d19d34e64da62496c6d34
Mateo <pannetier.mateo@gmail.com>
2025-09-19 16:26:05 +0200

    first commit for the TP

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ue-ad-a1-rest!")
+
+
+if __name__ == "__main__":
+    main()
--- movie/movie.py
+++ movie/movie.py
@@ -23,6 +23,68 @@ def write(movies):
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie),200)
+            return res
+    return make_response(jsonify({"error":"Movie ID not found"}),500)
+
+@app.route("/moviesbytitle", methods=['GET'])
+def get_movie_bytitle():
+    json = ""
+    if request.args:
+        req = request.args
+        for movie in movies:
+            if str(movie["title"]) == str(req["title"]):
+                json = movie
+
+    if not json:
+        res = make_response(jsonify({"error":"movie title not found"}),500)
+    else:
+        res = make_response(jsonify(json),200)
+    return res
+
+@app.route("/movies/<movieid>", methods=['POST'])
+def add_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            print(movie["id"])
+            print(movieid)
+            return make_response(jsonify({"error":"movie ID already exists"}),500)
+
+    movies.append(req)
+    write(movies)
+    res = make_response(jsonify({"message":"movie added"}),200)
+    return res
+
+@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+def update_movie_rating(movieid, rate):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movie["rating"] = rate
+            res = make_response(jsonify(movie),200)
+            write(movies)
+            return res
+
+    res = make_response(jsonify({"error":"movie ID not found"}),500)
+    return res
+
+@app.route("/movies/<movieid>", methods=['DELETE'])
+def del_movie(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movies.remove(movie)
+            write(movies)
+            return make_response(jsonify(movie),200)
+
+    res = make_response(jsonify({"error":"movie ID not found"}),500)
+    return res
+
 if __name__ == "__main__":
     #p = sys.argv[1]
     print("Server running in port %s"%(PORT))
